2f5e4046380b341c1342d34d4a3ec74b6bba720b
ozwv <nayanako03@naver.com>
2023-05-18 23:01:29 +0900

    230518_OSS_Homework#4

--- figure.py
+++ figure.py
@@ -4,7 +4,7 @@
 
 figure 모듈은 그림과 관련된 함수 및 클래스를 제공하는 모듈입니다.
 line class를 이용하여 선의 길이를 설정/참조를 수행하며 
-area_squre, area_circle, area_regular_triangle 함수로 
+area_square, area_circle, area_regular_triangle 함수로 
 각각 정사각형, 원, 정삼각형의 넓이를 반환합니다.
 """
 
@@ -20,12 +20,12 @@ class line:
     set_length와, get_length 메소드를 제공합니다.
     """
     __length = 0
-    def __init__(self, legnth):
+    def __init__(self, length):
         """생성자 초기 length 값을 받습니다.
         Args:
             length (int or float): 초기 선의 길이 값입니다.
         """
-        self.__length = legnth
+        self.__length = length
     
     def set_length(self, length):
         """선의 길이를 수정합니다.
@@ -44,7 +44,7 @@ class line:
 def area_square(length):
     """ 길이를 입력받아 정사각형의 넓이를 구하는 함수입니다.
     Args:
-        length( int or float): 한 변의 길이입니다.
+        length(int or float): 한 변의 길이입니다.
     Returns:
         int or float: 정사각형의 넓이를 반환합니다.
     """
@@ -55,7 +55,7 @@ def area_circle(length):
     Args:
         length (int or float) : 한 변의 길이입니다.
     Returns:
-        int or float: 정삼각형의 넓이를 반환합니다. 
+        int or float: 원의 넓이를 반환합니다. 
     """
     return length * length * math.pi
 
@@ -63,7 +63,7 @@ def area_regular_triangle(length):
     """길이를 입력받아 정삼각형의 넓이를 구하는 함수입니다.
     Args:
         length (int or float): 한 변의 길이입니다.
-    Retruns:
+    Returns:
         int or float: 정삼각형의 넓이를 반화합니다.
     """
     return length * length * math.sqrt(3) / 4
